53a0d1837b91a316ab7bae9f5882477423070243
jokbaltime <cadlims@gmail.com>
2026-07-24 16:47:25 +0900

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,64 +1,98 @@
-// =====================================
-// JOKBALTlME FIREBASE CONFIG
-// =====================================
+// ======================================
+// JOKBALTIME FIREBASE CONFIG
+// ======================================
 
-import { initializeApp } from 
+
+import {
+    initializeApp
+}
+from
 "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 
-import { 
-getFirestore,
-doc,
-setDoc,
-getDoc,
-onSnapshot,
-collection,
-addDoc,
-serverTimestamp
+
+
+import {
+
+    getFirestore,
+    doc,
+    setDoc,
+    getDoc,
+    deleteDoc,
+    collection,
+    query,
+    where,
+    getDocs
+
 }
-from 
+from
 "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+
 
 
 // Firebase 설정
 
 const firebaseConfig = {
 
+
 apiKey: "AIzaSyAykACgg1IqU-8tawPLRAfP2pKYST-PWjQ",
 
-authDomain: "jokbaltime-coupon.firebaseapp.com",
+authDomain:
+"jokbaltime-coupon.firebaseapp.com",
 
-projectId: "jokbaltime-coupon",
+projectId:
+"jokbaltime-coupon",
 
-storageBucket: "jokbaltime-coupon.firebasestorage.app",
+storageBucket:
+"jokbaltime-coupon.firebasestorage.app",
 
-messagingSenderId: "1077568919018",
+messagingSenderId:
+"1077568919018",
 
-appId: "1:1077568919018:web:2205193cae7848ec518e93",
+appId:
+"1:1077568919018:web:2205193cae7848ec518e93",
 
-measurementId: "G-BEHR2WZR0F"
+measurementId:
+"G-BEHR2WZR0F"
 
 };
 
 
+
+
 // Firebase 시작
 
-const app = initializeApp(firebaseConfig);
+const app =
+initializeApp(firebaseConfig);
 
 
-const db = getFirestore(app);
+
+const db =
+getFirestore(app);
 
 
-// 다른 파일에서 사용 가능하도록 전달
+
+
+// export
 
 export {
 
-db,
-doc,
-setDoc,
-getDoc,
-onSnapshot,
-collection,
-addDoc,
-serverTimestamp
+    db,
+
+    doc,
+
+    setDoc,
+
+    getDoc,
+
+    deleteDoc,
+
+    collection,
+
+    query,
+
+    where,
+
+    getDocs
 
 };
