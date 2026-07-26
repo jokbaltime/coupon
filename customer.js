a6d4c8b6fb097ab71fbe51c9bd94d47d0b38f99c
jokbaltime <cadlims@gmail.com>
2026-07-26 12:58:22 +0900

    Update customer.js

--- customer.js
+++ customer.js
@@ -1,1 +1,100 @@
+import {
 
+db,
+addDoc,
+collection,
+serverTimestamp
+
+} from "./firebase.js";
+
+
+console.log("customer.js 실행");
+
+
+
+const couponInput =
+document.getElementById("couponNumber");
+
+
+const requestButton =
+document.getElementById("requestButton");
+
+
+const result =
+document.getElementById("result");
+
+
+
+requestButton.onclick = async()=>{
+
+
+const number =
+couponInput.value.trim();
+
+
+
+if(!number){
+
+result.innerHTML =
+"쿠폰번호를 입력하세요";
+
+return;
+
+}
+
+
+
+try{
+
+
+await addDoc(
+
+collection(
+db,
+"coupon_request"
+),
+
+{
+
+
+couponNumber:number,
+
+
+status:"waiting",
+
+
+requestTime:
+serverTimestamp()
+
+
+}
+
+);
+
+
+
+result.innerHTML =
+"✅ 직원 승인 요청 완료";
+
+
+couponInput.value="";
+
+
+}
+
+
+catch(error){
+
+
+console.error(error);
+
+
+result.innerHTML =
+"요청 오류 : "
++error.message;
+
+
+}
+
+
+};
