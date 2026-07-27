e1f01e4df7c5fcc0ace02fadc63a38fce1e81fee
jokbaltime <cadlims@gmail.com>
2026-07-27 16:35:44 +0900

    Update admin.js

--- admin.js
+++ admin.js
@@ -273,7 +273,29 @@ function renderCouponList(){
 couponList.innerHTML="";
 
 
-couponSnapshotData.forEach((data)=>{
+const sortedList = [...couponSnapshotData].sort((a,b)=>{
+
+
+const aTime =
+a.updatedAt?.seconds ||
+a.createdAt?.seconds ||
+0;
+
+
+const bTime =
+b.updatedAt?.seconds ||
+b.createdAt?.seconds ||
+0;
+
+
+return bTime - aTime;
+
+
+});
+
+
+
+sortedList.forEach((data)=>{
 
 
 if(
