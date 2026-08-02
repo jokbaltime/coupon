9b7233e678d43350c98a6bef0cf256ef6a62befd
jokbaltime <cadlims@gmail.com>
2026-08-02 14:59:24 +0900

    Update admin.js

--- admin.js
+++ admin.js
@@ -536,9 +536,22 @@ div.innerHTML=
 
 <p>
 상태 :
-${statusText}
+${
+data.status==="issued"
+?"✅ 사용 가능"
+:
+data.status==="used"
+?"❌ 사용 완료"
+:
+data.status==="approved"
+?"✅ 승인 완료"
+:
+data.status==="waiting"
+?"⏳ 승인 대기"
+:
+"-"
+}
 </p>
-
 <p>
 할인 :
 ${data.discount || 0}%
